86e650fc899666561a86b40cf625bbfff5b55def
ArtemLakhurov <lakhurov@gmail.com>
2020-02-10 17:34:57 +0200

    Bug fix

--- src/Components/Dashboard/listItems.js
+++ src/Components/Dashboard/listItems.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import LayersIcon from '@material-ui/icons/Layers';
-import AssignmentIcon from '@material-ui/icons/Assignment';
 import {
 	Link
 } from 'react-router-dom'
